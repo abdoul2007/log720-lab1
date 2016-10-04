6654dc7eb368a7c64ee1a13d8cccea93eda5bfa5
Aziz Soumahoro ABDOUL <azizsoumahoro.abdoul@ville.montreal.qc.ca>
2016-10-04 15:18:16 -0400

    start to implement corba objects

---
+++ equipe15-log720-A16-lab1/serveur-poste/src/main/java/ca/etsmtl/log720/lab1/DossierImpl.java
@@ -0,0 +1,15 @@
+package ca.etsmtl.log720.lab1;
+
+import java.util.ArrayList;
+
+public class DossierImpl{
+	private int id;
+    private String nom;
+    private String noPermis;
+    private String noPlaque;
+    private String prenom;
+    private int niveau;
+    private ArrayList<Integer> listeInfraction;
+    private ArrayList<Integer> listeReaction;
+
+}
