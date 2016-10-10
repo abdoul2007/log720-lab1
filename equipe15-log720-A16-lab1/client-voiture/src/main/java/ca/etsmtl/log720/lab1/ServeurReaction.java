7b0c8c2bfe09e62a4c3ac5bc4a673c9af870ff44
adilbelhachemi <adil.belhachemi@gmail.com>
2016-10-09 20:09:39 -0400

    serveurReaction

---
+++ equipe15-log720-A16-lab1/client-voiture/src/main/java/ca/etsmtl/log720/lab1/ServeurReaction.java
@@ -0,0 +1,45 @@
+package ca.etsmtl.log720.lab1;
+
+
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+
+public class ServeurReaction {
+
+
+	protected static org.omg.PortableServer.POA _poa;
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(args, null);
+
+		try {
+			org.omg.PortableServer.Servant servant;
+			org.omg.CORBA.Object o;
+
+			// Initialize POA
+			_poa = org.omg.PortableServer.POAHelper
+					.narrow(orb.resolve_initial_references("RootPOA"));
+			_poa.the_POAManager().activate();
+
+			// Initialize servant (Remote Object), convert to CORBA reference
+			servant = new BanqueReactionImpl();
+			o = _poa.servant_to_reference(servant);
+
+			// Register Rermote Object with naming context
+			NamingContextExt nc = NamingContextExtHelper.narrow(orb
+					.resolve_initial_references("NameService"));
+			NameComponent[] name = new NameComponent[] { new NameComponent(
+					"BanqueReaction", "service") };
+			nc.rebind(name, o);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// Start the ORB main thread
+		orb.run();
+	}
+
+}
